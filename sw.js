bd02666d011131231f8f0302f5d23ca83d875c4b
MejriZiad <mejri.ziad@gmail.com>
2026-02-18 17:43:10 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 /**
 	* Tests Hub - Service Worker
-	* Version: v2.3.1
+	* Version: v2.3.2
 	* Features: Offline support, Background sync, Push notifications, Periodic sync
 */
 
-const CACHE_NAME = 'tests-hub-v2.3.1';
-const STATIC_CACHE_NAME = 'tests-hub-static-v2.3.1';
-const DYNAMIC_CACHE_NAME = 'tests-hub-dynamic-v2.3.1';
+const CACHE_NAME = 'tests-hub-v2.3.2';
+const STATIC_CACHE_NAME = 'tests-hub-static-v2.3.2';
+const DYNAMIC_CACHE_NAME = 'tests-hub-dynamic-v2.3.2';
 
 // Files to cache on install (app shell)
 const STATIC_ASSETS = [
